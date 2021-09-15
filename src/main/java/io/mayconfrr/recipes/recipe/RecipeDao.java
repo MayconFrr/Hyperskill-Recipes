24e1127e476ee1fdb295a83ed054f37c357d9c50
MayconFrr <32937928+MayconFrr@users.noreply.github.com>
2021-09-15 13:45:57 -0300

    Update Recipe Service to return required data

---
+++ src/main/java/io/mayconfrr/recipes/recipe/RecipeDao.java
@@ -0,0 +1,7 @@
+package io.mayconfrr.recipes.recipe;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecipeDao {
+}
--- src/main/java/io/mayconfrr/recipes/recipe/RecipeService.java
+++ src/main/java/io/mayconfrr/recipes/recipe/RecipeService.java
@@ -1,16 +1,20 @@
 package io.mayconfrr.recipes.recipe;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class RecipeService {
+    private final RecipeDao recipeDao;
+
     public Long saveRecipe(Recipe recipe) {
-        return null;
+        return recipeDao.save(recipe);
     }
 
     public Optional<Recipe> getRecipeById(Long id) {
-        return Optional.empty();
+        return recipeDao.findById(id);
     }
 }

dc66cb338f7400519f56c77cd260c3be1598008e
MayconFrr <32937928+MayconFrr@users.noreply.github.com>
2021-09-15 15:53:33 -0300

    Update Recipe Model as JPA Entity

--- src/main/java/io/mayconfrr/recipes/recipe/Recipe.java
+++ src/main/java/io/mayconfrr/recipes/recipe/Recipe.java
@@ -4,15 +4,29 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Data
+@Entity
 @NoArgsConstructor
 public class Recipe {
     @JsonIgnore
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String description;
+
+    @ElementCollection
+    @Column(nullable = false)
     private List<String> ingredients;
+
+    @ElementCollection
+    @Column(nullable = false)
     private List<String> directions;
 }

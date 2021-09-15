90b1d588c87c215da2c632ac12212cdf6d5c0709
MayconFrr <32937928+MayconFrr@users.noreply.github.com>
2021-09-15 13:54:07 -0300

    Add Recipe DAO

--- src/main/java/io/mayconfrr/recipes/recipe/Recipe.java
+++ src/main/java/io/mayconfrr/recipes/recipe/Recipe.java
@@ -1,11 +1,14 @@
 package io.mayconfrr.recipes.recipe;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class Recipe {
+    @JsonIgnore
+    private Long id;
     private String name;
     private String description;
     private String ingredients;
--- src/main/java/io/mayconfrr/recipes/recipe/RecipeDao.java
+++ src/main/java/io/mayconfrr/recipes/recipe/RecipeDao.java
@@ -2,6 +2,27 @@ package io.mayconfrr.recipes.recipe;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Component
 public class RecipeDao {
+    private final List<Recipe> recipes = new ArrayList<>();
+    private final AtomicLong atomicLong = new AtomicLong(1);
+
+    public Optional<Recipe> findById(Long id) {
+        return recipes.stream()
+                .filter(recipe -> Objects.equals(recipe.getId(), id))
+                .findFirst();
+    }
+
+    public Long save(Recipe recipe) {
+        long id = atomicLong.getAndIncrement();
+        recipe.setId(id);
+        recipes.add(recipe);
+        return id;
+    }
 }
